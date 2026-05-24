0e21c08a2a177a90ada90c0cce7229c7e27e81bb
khromasprojets-jpg <khromas.projets@gmail.com>
2026-05-24 18:22:03 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useCallback, useRef } from "react";
 import { createClient } from "@supabase/supabase-js";
 
+
 // ─── CONFIG SUPABASE ──────────────────────────────────────────────────────────
 const SUPABASE_URL = "https://fgzulqopyxtfmtlfctxx.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_PU9sf0pSZA6d0eqZDs8Xgg_gNp2LsSQ";
